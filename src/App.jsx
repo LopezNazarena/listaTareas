6e75443a66d0fd8d4ca88b4d33520289d59406b9
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-10-31 15:44:08 -0300

    agregamos components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import FormularioTarea from "./components/FormularioTarea";
+import ListaTarea from "./components/ListaTarea";
+// importar la hoja de estilo
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <>
-      <h1>Lista de tareas</h1>
-      <hr />
+      <Container>
+        <h1 className="display-4">Lista de tareas</h1>
+        <hr />
+        <FormularioTarea></FormularioTarea>
+        <ListaTarea></ListaTarea>
+      </Container>
     </>
   );
 }
-
 
 export default App;
